f3735e3c89a9e845858223234f8317d8e5a11a86
Alfred Neumayer <dev.beidl@gmail.com>
2021-04-23 19:21:29 +0200

    deps: Add dependency install scripts

---
+++ deps/dnf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo dnf install -y \
+    boost-devel \
+    libxkbcommon-devel \
+    wayland-devel \
+    glib2-devel \
+    gobject-introspection-devel \
+    lttng-ust-devel \
+    json-glib-devel \
+    zeitgeist-devel \
+    libgee-devel \
+    vala \
+    dbus-devel \
+    dbus-tests
