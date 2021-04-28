b95fc26c6c4bf81e363808e932efa42678128611
Alfred Neumayer <dev.beidl@gmail.com>
2021-04-28 21:05:31 +0200

    test: Use glib-compile-schemas

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/opt/justSignage-current/lib
-export QML2_IMPORT_PATH=/opt/justSignage-current/lib/qml:/opt/justSignage-current/lib/qt5/qml
-export QT_QPA_PLATFORM_PLUGIN_PATH=/opt/justSignage-current/lib/qt5/plugins/platforms
+export LD_LIBRARY_PATH=/opt/justSignage-0.0.0-rc0/lib
+export QML2_IMPORT_PATH=/opt/justSignage-0.0.0-rc0/lib/qml:/opt/justSignage-0.0.0-rc0/lib/qt5/qml
+export QT_QPA_PLATFORM_PLUGIN_PATH=/opt/justSignage-0.0.0-rc0/lib/qt5/plugins/platforms
 
 SRC_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SRC_PATH
 
-env QT_QPA_PLATFORM=mirserver MIR_SERVER_CURSOR=null src/build/compositor/src &
+sudo glib-compile-schemas /opt/justSignage-0.0.0-rc0/share/glib-2.0/schemas
+env \
+    GSETTINGS_SCHEMA_DIR=/opt/justSignage-0.0.0-rc0/share/glib-2.0/schemas \
+    QT_QPA_PLATFORM=mirserver \
+    MIR_SERVER_CURSOR=null \
+    src/build/compositor/src &
 
 sleep 3
 
