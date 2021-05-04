fef1cb129d6d26ee912e5e6735d78f6a5bf45266
Alfred Neumayer <dev.beidl@gmail.com>
2021-05-04 21:26:51 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -13,11 +13,11 @@ export QT_QPA_PLATFORM_PLUGIN_PATH=$INSTALL/lib/qt5/plugins/platforms
 
 sudo glib-compile-schemas $INSTALL/share/glib-2.0/schemas
 env \
-    MIR_SERVER_X11_OUTPUT=800x1200:800x1200 \
+    MIR_SERVER_X11_OUTPUT=1500x1200:1500x1200 \
     GSETTINGS_SCHEMA_DIR=$INSTALL/share/glib-2.0/schemas \
     QT_QPA_PLATFORM=mirserver \
     MIR_SERVER_CURSOR=null \
-    src/build/compositor/justsignage-compositor &
+    $INSTALL/bin/justsignage-compositor &
 
 sleep 3
 
@@ -26,7 +26,7 @@ sleep 2
 QT_QPA_PLATFORM=wayland ./src/build/webplayer/webplayer &
 #QT_QPA_PLATFORM=wayland ./src/build/videoplayer/videoplayer &
 QT_QPA_PLATFORM=wayland ./src/build/qtavplayer/qtavplayer &
-mpv --geometry=2400x1400 --gpu-context=wayland '/home/alfred/Videos/4k.mkv'
+#mpv --geometry=2400x1400 --gpu-context=wayland '/home/alfred/Videos/4k.mkv'
 
 for job in `jobs -p`; do
     wait $job
